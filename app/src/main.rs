9590b95f8b6be7bb42917fe9ef2dcdb4a4dd44ed
Breyner Mola <p.3dstudio21_1@sic.gov.co>
2022-07-27 10:45:19 -0500

    actix-web

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Surprise Motherfuckers!");
+#[macro_use]
+extern crate diesel;
+
+pub mod models;
+pub mod schema;
+
+use dotenv::dotenv;
+use std::env;
+
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+
+use diesel::r2d2::ConnectionManager;
+use diesel::r2d2::Pool;
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("0.0.0.0", 9900))?
+    .run()
+    .await
 }
